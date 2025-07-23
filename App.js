4127c33ae42bcb4b840284c1ae43a86afe726598
Rennan <rennan-castanheira@hotmail.com>
2025-07-23 12:02:16 -0300

    toast

--- App.js
+++ App.js
@@ -1,35 +1,59 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import Routes from './src/routes/routes';
-import Toast from 'react-native-toast-message';
-import { notificacaoToastCustomizado } from './src/components/notificacaoToastCustomizado';
-import { LocaleConfig } from 'react-native-calendars';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes/routes";
+import Toast from "react-native-toast-message";
+import { notificacaoToastCustomizado } from "./src/components/notificacaoToastCustomizado";
+import { LocaleConfig } from "react-native-calendars";
 
 export default function App() {
-  LocaleConfig.locales['pt-br'] = {
+  LocaleConfig.locales["pt-br"] = {
     monthNames: [
-      'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-      'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
     ],
     monthNamesShort: [
-      'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun',
-      'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
+      "Jan",
+      "Fev",
+      "Mar",
+      "Abr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Set",
+      "Out",
+      "Nov",
+      "Dez",
     ],
     dayNames: [
-      'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira',
-      'Quinta-feira', 'Sexta-feira', 'Sábado'
+      "Domingo",
+      "Segunda-feira",
+      "Terça-feira",
+      "Quarta-feira",
+      "Quinta-feira",
+      "Sexta-feira",
+      "Sábado",
     ],
-    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
-    today: 'Hoje'
+    dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"],
+    today: "Hoje",
   };
 
-  LocaleConfig.defaultLocale = 'pt-br';
+  LocaleConfig.defaultLocale = "pt-br";
 
   return (
     <NavigationContainer>
       <Routes />
-      <Toast config={notificacaoToastCustomizado} visibilityTime={3000} autoHide position="top" />
+      <Toast visibilityTime={3000} autoHide position="top" />
     </NavigationContainer>
   );
 }
-

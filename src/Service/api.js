335a08ee6438bbbeede6bdafff84079814c328e3
Rennan <rennan-castanheira@hotmail.com>
2026-02-23 09:22:31 -0300

    ajuste

--- src/Service/api.js
+++ src/Service/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: 'https://pet-on-api.azurewebsites.net/api'
-  baseURL:
-    "https://peton-api-prod-c8bwfnfqghgaccdn.brazilsouth-01.azurewebsites.net/api/",
+  // baseURL:
+  //   "https://peton-api-prod-c8bwfnfqghgaccdn.brazilsouth-01.azurewebsites.net/api/",
+  baseURL: "http://18.230.92.28/api/",
 });
 
 export default api;
